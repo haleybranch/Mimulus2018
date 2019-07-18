35eecd71008151a5053619284ac4c4740dce4d79
haleybranch <haley.branch@biodiversity.ubc.ca>
2019-07-17 17:01:17 -0700

    finished the gsw section

--- mcardinalis_physiology.R
+++ mcardinalis_physiology.R
@@ -151,8 +151,8 @@ library(lmerTest)
 CIs <- confint(gsw9.cmd, method = "profile")
 summary(gsw9.cmd) ## pvalue is <2e-16 for gsw9.cmd --> treatment significant
 
-
-# make new column grouping pre-drought (2010 and 2011) and post drought (2015-2016)
+# Adding this section to see what happens when we group years before,during,and post drought
+## make new column grouping pre-drought (2010 and 2011) and post drought (2015-2016)
 mydata <- mydata %>%
   mutate(PrePost = ifelse(Year == "2010", "1", 
                           ifelse(Year == "2011", "1", 
@@ -161,144 +161,53 @@ mydata <- mydata %>%
                                  ifelse(Year == "2014", "2",
                                  ifelse(Year == "2015", "3",
                                         ifelse(Year == "2016", "3", NA))))))))
+mydata$PrePost <- as.factor(mydata$PrePost)
 
-######## are there any group differences, regardless of climate/weather?
+
+## are there any group differences, regardless of climate/weather?
 gsw1.group= lmer(gsw ~ Treatment*Site*PrePost + (1|Year), mydata)
 summary(gsw1.group)
-anova(gsw1.group) #treatment, site:prepost significant
+anova(gsw1.group) #treatment, site:prepost significant, trt:site
 
 # drop 3-way
-Vcmax2.group= lmer(Vcmax ~ Treatment*Site + Site*PrePost + Treatment*PrePost + (1|Year), mydata)
-summary(Vcmax2.group)
-anova(Vcmax2.group) 
+gsw2.group= lmer(gsw ~ Treatment*Site + Site*PrePost + Treatment*PrePost + (1|Year), mydata)
+summary(gsw2.group)
+anova(gsw2.group) 
 
-model.sel(Vcmax1.group, Vcmax2.group)
-#wow, model with 3-way interaction is highly favored over reduced model
-visreg(Vcmax1.group, xvar="PrePost", by="Site", cond=list(Treatment="D"))
-visreg(Vcmax1.group, xvar="PrePost", by="Site", cond=list(Treatment="W"))
-visreg(Vcmax1.group, xvar="PrePost", by="Site", cond=list(Treatment=c("W", "D")))
+model.sel(gsw1.group, gsw2.group)
+
+#reduced model favored 
+visreg(gsw2.group, xvar="PrePost", by="Site", cond=list(Treatment="D"))
+visreg(gsw2.group, xvar="PrePost", by="Site", cond=list(Treatment="W"))
+visreg(gsw2.group, xvar="PrePost", by="Site", cond=list(Treatment=c("W", "D")))
 
 # Can we sub Latitude for Site to make interpretation easier? 
 # Don't use this one
-Vcmax1.lat= lmer(Vcmax ~ Treatment*Latitude*PrePost + (1|Year), mydata)
-summary(Vcmax1.lat)
-anova(Vcmax1.lat) #3-way not significant 
-visreg(Vcmax1.lat, xvar="Latitude", by="PrePost", overlay=T, cond=list(Treatment="D"))
-visreg(Vcmax1.lat, xvar="Latitude", by="PrePost", overlay=T, cond=list(Treatment="W"))
+gsw1.lat= lmer(gsw ~ Treatment*Latitude*PrePost + (1|Year), mydata)
+summary(gsw1.lat)
+anova(gsw1.lat) #3-way not significant 
+visreg(gsw1.lat, xvar="Latitude", by="PrePost", overlay=T, cond=list(Treatment="D"))
+visreg(gsw1.lat, xvar="Latitude", by="PrePost", overlay=T, cond=list(Treatment="W"))
 
 ## drop 3-way
-Vcmax2.lat= lmer(Vcmax ~ Treatment*Latitude + Latitude*PrePost + Treatment*PrePost + (1|Year), mydata)
-summary(Vcmax2.lat)
-anova(Vcmax2.lat) 
+gsw2.lat= lmer(gsw ~ Treatment*Latitude + Latitude*PrePost + Treatment*PrePost + (1|Year), mydata)
+summary(gsw2.lat)
+anova(gsw2.lat) 
 
-model.sel(Vcmax1.lat, Vcmax2.lat) #about 50:50 model weight, AIC almost tied 
+model.sel(gsw1.lat, gsw2.lat) #about 50:50 model weight, AIC almost tied 
 
 # Can we sub CMD for Site to make interpretation easier?
-Vcmax1.cmd= lmer(Vcmax ~ Treatment*CMD.clim.scaled*PrePost + (1|Year), mydata)
-summary(Vcmax1.cmd)
-anova(Vcmax1.cmd) #3-way not significant 
-visreg(Vcmax1.cmd, xvar="CMD.clim.scaled", by="PrePost", overlay=T, cond=list(Treatment="D"))
-visreg(Vcmax1.lat, xvar="CMD.clim.scaled", by="PrePost", overlay=T, cond=list(Treatment="W"))
+gsw1.cmd= lmer(gsw~ Treatment*CMD.clim.scaled*PrePost + (1|Year), mydata)
+summary(gsw1.cmd)
+anova(gsw1.cmd) #3-way not significant 
+visreg(gsw1.cmd, xvar="CMD.clim.scaled", by="PrePost", overlay=T, cond=list(Treatment="D"))
+visreg(gsw1.lat, xvar="CMD.clim.scaled", by="PrePost", overlay=T, cond=list(Treatment="W"))
 
 # drop 3-way
-Vcmax2.cmd= lmer(Vcmax ~ Treatment*CMD.clim.scaled + CMD.clim.scaled*PrePost + Treatment*PrePost + (1|Year), mydata)
-summary(Vcmax2.cmd)
-anova(Vcmax2.cmd) 
+gsw2.cmd= lmer(gsw ~ Treatment*CMD.clim.scaled + CMD.clim.scaled*PrePost + Treatment*PrePost + (1|Year), mydata)
+summary(gsw2.cmd)
+anova(gsw2.cmd) 
 
-model.sel(Vcmax1.cmd, Vcmax2.cmd) #strong support for 3-way interaction
+model.sel(gsw1.cmd, gsw2.cmd) #strong support for 3-way interaction
 
-
-#### Water potential
-wpotential <- na.omit(data.frame(read.csv("waterpotentialJuly2018.csv"))) # red sensor 
-head(wpotential)
-tail(wpotential)
-
-# orient data longwise
-wpotential <- wpotential %>%
-  gather(Bench, WP, WP1:WP4)
-head(wpotential)
-
-# add AM/PM to time 
-wpotential <- wpotential %>% 
-  mutate(Time2 = paste(Time, AMPM),
-         DateTime = paste(Measurement, Time2))
-head(wpotential)
-
-# change format to 24 hour, convert format for ggplot
-wpotential$Time3 <- as.POSIXct(strptime(wpotential$DateTime, "%Y-%m-%d %I:%M:%S %p"))
-wpotential$Day <- as.POSIXct(wpotential$Measurement)
-head(wpotential, 20)
-str(wpotential)
-
-# remove old date and time columns, 
-wpotential <- wpotential %>% 
-  select(Day, DayTime = Time3, Bench, WP)
-head(wpotential)
-tail(wpotential)
-
-# graph of raw WP by bench
-ggplot(data=wpotential, aes(x=DayTime, y=WP)) +
-  geom_point(aes(color=Bench)) 
-
-# mean per day 
-wpotential.mean <- wpotential %>%
-  group_by(Bench, Day) %>%
-  summarise(WP.mean = mean(WP)) %>% 
-  ungroup() %>% 
-  select(Bench, Day, WP.mean)
-head(wpotential.mean, 10)
-
-# graph of mean wp per day
-ggplot(data=wpotential.mean, aes(x=Day, y=WP.mean)) +
-  geom_point(aes(color=Bench))
-
-## amy stopped here
-
-
-##### creating new subsets for days of interest
-#attempt 1
-wpotential.start <- wpotential.mean %>%
-  group_by(Measurement)
-wpotential.start
-
-#attempt 2
-wpotential.mean$Measurement <- as.numeric(wpotential.mean$Measurement)
-drought.start <- wpotential.mean[wpotential$Measurement == c(2018-07-01, 2018-07-14),]
-drought.start <- data.frame(Measurement = c(2018-07-01, 2018-07-14))
-
-#attempt3
-plot(wpotential.mean$Time[wpotential.mean$Measurement < 2018-07-14],
-     wpotential.mean$WP[wpotential.mean$Measurement < 2018-07-14])
-
-#attempt 4
-drought.start <- subset(wpotential2.mean, wpotential2.mean$Measurement > 2018-07-01 & wpotential2.mean$Measurement < 2018-07-15)
-
-#attempt 5
-replace.value(wpotential.mean, Measurement, from = 2018-07-01, to=as.factor(1), verbose = FALSE)
-wpotential.mean
-
-#attempt 6
-wpotential.mean$Measurement <- as.character(wpotential.mean$Measurement)
-
-#attempt 7
-# create a new column using mutate with an if else statement 
-# or split the measurement column into year, month, day -- and use if the day is less than 14 ...
-# splitstring function?
-
-#step 1 - separate YYYY-MM-DD into columns
-wpotential.mean <- separate(wpotential.mean, "Measurement", c("Year", "Month", "Day"), sep = "-")
-
-#step 2 - create new column with 3 stages: 1 = 3/4 water, 2 = 1/2 water, 3 = 0 water
-wpot <- as.data.frame(wpotential.mean)
-wpot$Day <- as.numeric(wpot$Day)
-
-wpot$Stage[wpot$Day<=14]<-1
-wpot$Stage[wpot$Day>14 & wpot$population<30] <-2
-wpot$Stage[wpot$Day>=30] <-3
-wpot$Stage <- as.character(wpot$Stage)
-
-#graph the daily water potential during each stage 
-ggplot(data=wpot, aes(x=Time, y=WP.mean)) +
-  geom_point(aes(color=Bench)) +
-  facet_wrap(aes(group=Stage))
 
